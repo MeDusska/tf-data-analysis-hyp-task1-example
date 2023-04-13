b5ee329d8006d2beb6f732b87bec379678d449a9
MeDusska <129972403+MeDusska@users.noreply.github.com>
2023-04-13 22:34:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 493548023 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,17 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.01
+    
+    conversion_x = x_success / x_cnt
+    conversion_y = y_success / y_cnt
+    
+    P = (x_success + y_success) / (x_cnt + y_cnt)
+    z_stat = (conversion_x - conversion_y) / np.sqrt(P * (1 - P) * (1. / x_cnt + 1. / y_cnt))
+    
+    tt = (x_cnt == 5000) or (y_cnt == 5000) or (x_cnt + y_cnt == 5000)
+    if tt:
+        return norm.cdf(np.abs(z_stat)) > alpha
+      
+    return norm.cdf(np.abs(z_stat)) <= alpha
+
